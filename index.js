b138b0073b84d565aa3a4824388895fb8cbd8ddc
nataliavega <nvegar@gmail.com>
2022-01-06 11:35:13 +0100

    modified port for backend node and import express

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require ('express')
+import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello')
 })
 
-app.listen(3000)
+app.listen(8000)
 
-console.log("server is running on localhost://3000")
+console.log("server is running on http://localhost:8000/")
