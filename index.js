9ac7647eecf56fa62726ea76f471a0a25e56e227
anshultmr <anshltmr@outlook.com>
2022-01-19 17:49:44 +0100

    mongoose installation

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
   res.send('Hello')
 })
 
+app.get('/Anshul', (req, res) => {
+  res.send('Anshul is great')
+})
 app.listen(8000)
 
 console.log("server is running on http://localhost:8000/")
