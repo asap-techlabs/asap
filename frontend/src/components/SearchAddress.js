03e8de96b9d4e4f18221e9e6f82c9e0c65b18c39
nataliavega <nvegar@gmail.com>
2022-02-10 11:56:42 +0100

    Revert "passing data'"
    This reverts commit 852d5283ad73d5225d4367e9b1de08693fcf5cdf.

--- frontend/src/components/SearchAddress.js
+++ frontend/src/components/SearchAddress.js
@@ -1,57 +1,22 @@
-import { useState } from 'react'
 import Address from './Address'
 
-// this component incorporates the search of addresses on the form calling the API.
+// this components incorporates the search of addresses on the form.
 
-export default function SearchAddress(props) {
-
-  const [state, setState] = useState({
-      originAddress: props,
-      destinationAddress: '',
-      distance: 0,
-      date: new Date(),
-      price: 0
-      });
-
-    function onChangeOriginAddress(props) {
-      this.setState({
-        originAddress: props.target.value
-      });
-    };
-
-    function onChangeDestinationAddress(props) {
-    this.setState({
-      destinationAddress: props.target.value
-    });
-    }
-
-    function onSubmit(event) {
-  event.preventDefault();
-
-  const order = {
-    originAddress: state.originAddress,
-    destinationAddress: state.destinationAddress,
-    distance: state.distance,
-    date: state.date,
-    price: state.price,
-  }
-  console.log(order);
-}
+const SearchAddress = () => {
   return (
   <div className="container-md" style={{marginTop: "30px"}}>
-    <h2 className="title-search-form">Where? </h2>
-    <form onSubmit={onSubmit} className="searchForm" style={{marginTop: "30px"}}>
+    <h2 className="title-search-form">Where?</h2>
+    <form className="searchForm" style={{marginTop: "30px"}}>
       <div className="row mb-3">
         <label htmlFor="from" className="col-sm-2 col-form-label">From:</label>
         <div className="col-sm-10">
-          <Address type='text' onPlaceSelect={onChangeOriginAddress}/>
+          <Address/>
         </div>
       </div>
       <div className="row mb-3">
         <label htmlFor="to" className="col-sm-2 col-form-label">To:</label>
         <div className="col-sm-10">
-          <Address type='text' onPlaceSelect={onChangeDestinationAddress}
-          />
+          <Address/>
         </div>
       </div>
       <button type="submit" className="btn btn-success">Next</button>
@@ -59,3 +24,5 @@ export default function SearchAddress(props) {
   </div>
   )
 }
+
+export default SearchAddress
