bd5c71b2552e43b153e479589c0d9e925a5cb918
nataliavega <nvegar@gmail.com>
2022-02-18 22:45:01 +0100

    added styling to card recap. card is showing distance & price

--- frontend/src/components/Recap.js
+++ frontend/src/components/Recap.js
@@ -1,38 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { axios } from 'axios';
+import Order from "./Order.js"
 
-const Order = props => (
-
-<div>
-    <p>{props.order.originAddress}</p>
-    <p>{props.order.destinationAddress}</p>
-    <p>{props.order.distance} km</p>
-    {/* <p>{parseFloat(props.order.price.toFixed(2))}€</p> */}
-<Link to={"/edit/"+props.order._id}>edit</Link> | <Link to={"/delete/"+props.order._id}>delete</Link>
-
-</div>
-)
 
   export default class Recap extends Component {
     constructor(props) {
       super(props);
-
-
-    // this.deleteOrder = this.deleteOrder.bind(this)
-
     this.state = {
 
     };
   }
-
-
 
   componentDidMount() {
     this.getData();
   }
 
   getData = () => {
+    // this function gets the data from the database and set the values from there to the state.
     const id = window.location.pathname.split('/')[2];
     fetch('http://localhost:8000/orders/'+id)
       .then(data => data.json())
@@ -52,30 +35,21 @@ const Order = props => (
       });
   }
 
-  // deleteOrder(id) {
-  //   axios.delete(`http://localhost:8000/orders//${id}`)
-  //     .then(response => { console.log(response.data)});
-  //       this.props.history.push("/");
-  //   this.setState({
-  //     orders: this.state.orders.filter(el => el._id !== id)
-  //   })
-  // }
-
   orderShow() {
+    // as the order is stocked as an object in the state this function iterates
+    //over the object to get the values that are going to be passed and displayed in the Order
+    // component called  at the top.
     let order = {};
-    Object.entries(this.state).forEach(([key, value]) => order = value
-    );
+    Object.entries(this.state).forEach(([key, value]) => order = value);
     return <Order order={order} key={order.id}/>;
-    }
+  }
 
   render() {
-
+    // renders the information
     return (
-      <div>
-        <h3>Recap</h3>
-          <div>
+      <div className="card border-light mb-3" style={{marginTop: "30px"}}>
+        <h3 className="card-title">Your ride is almost ready !</h3>
           { this.orderShow() }
-          </div>
       </div>
     )
   }
