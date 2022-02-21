bb28795968b444e83069232655199a84dabcf737
soerenKrieger <94220224+soerenKrieger@users.noreply.github.com>
2022-02-21 20:10:48 +0100

    HomeScreen incl. Navbar and Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './Footer.css';
+
+function Footer () {
+	return (
+		<footer>
+			<p className="footer-copy">Copyright 2022 - Made during TechLabs Winter 2021/2022</p>
+		</footer>
+	)
+}
+
+export default Footer;
